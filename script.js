a948ac54fab96d6efea6ddfc74773bc3c730fce2
irmamrsn <irmamuresann@gmail.com>
2025-05-09 12:15:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Place your JavaScript in this file */
+
+document.getElementById("c1r1").addEventListener("click", function() {
+    console.log("Cell c1r1 clicked");
+    alert("Cell c1r1 clicked");
+});
+
